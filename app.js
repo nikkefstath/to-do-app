80ed94999b1bab9f79816de999569dadf151f68f
Nikki <naefstathiou@gmail.com>
2019-09-29 20:02:29 -0700

    Assignment 6 completed

--- app.js
+++ app.js
@@ -1,41 +1,61 @@
 function onReady(){
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
-  function createNewToDo () {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
 
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
-    
-    newToDoText.value = '';
-
-    renderTheUI();
-  }
-  function renderTheUI () {
+  function renderTheUI(){
     const toDoList = document.getElementById('toDoList');
 
     toDoList.textContent = '';
 
-    toDos.forEach(function(toDo) {
+    toDos.forEach(function(toDo){
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
-      newLi.textContent= toDo.title;
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Delete';
+
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+
+        renderTheUI();
+      });
+
+      newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
+    })
+
+  }
+function createNewToDo(){
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value){ return; }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id
     });
 
+ id++;
+
+    console.log(toDos);
+
+    newToDoText.value = '';
+
+    renderTheUI();
   }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
+    console.log(toDos);
   });
 
   renderTheUI();
